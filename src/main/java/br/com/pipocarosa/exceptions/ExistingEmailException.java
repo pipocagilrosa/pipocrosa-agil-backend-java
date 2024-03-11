49bc531d85a7a91ecd513dc8198a2bd0d42c4d3a
Gabriel <gabrielsantospds@hotmail.com>
2024-03-11 20:41:17 -0300

    delete outdated project

--- src/main/java/br/com/pipocarosa/exceptions/ExistingEmailException.java
+++
@@ -1,7 +0,0 @@
-package br.com.pipocarosa.exceptions;
-
-public class ExistingEmailException extends RuntimeException {
-    public ExistingEmailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/br/com/pipocarosa/exceptions/YoungUserException.java
+++
@@ -1,7 +0,0 @@
-package br.com.pipocarosa.exceptions;
-
-public class YoungUserException extends RuntimeException {
-    public YoungUserException(String message) {
-        super(message);
-    }
-}

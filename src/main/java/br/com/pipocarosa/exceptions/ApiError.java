56d17ce1d598c3dce312bc10f1061ff901efe4e1
Gabriel Santos <gabrielsantospds@hotmail.com>
2024-12-24 16:11:37 -0300

    fix: fix issue

--- src/main/java/br/com/pipocarosa/exceptions/ApiError.java
+++ src/main/java/br/com/pipocarosa/exceptions/ApiError.java
@@ -24,4 +24,36 @@ public class ApiError {
         this.message = message;
         this.path = path;
     }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
